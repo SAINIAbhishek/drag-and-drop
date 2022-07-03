8cbe5434bc0627bf3b50027d5ced1c6fbf7ef012
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-07-03 11:11:12 +0200

    header and refacotr.

---
+++ src/app/headers/header-kanban/header-kanban.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {KanbanService} from "../../services/kanban.service";
+import {MatDialog} from "@angular/material/dialog";
+import {ModalDialogComponent} from "../../modals/modal-dialog/modal-dialog.component";
+
+@Component({
+  selector: 'app-header-kanban',
+  templateUrl: './header-kanban.component.html',
+  styleUrls: ['./header-kanban.component.scss']
+})
+export class HeaderKanbanComponent implements OnInit {
+
+  constructor(private readonly dialog: MatDialog,
+              private readonly kanbanService: KanbanService) { }
+
+  ngOnInit(): void {
+  }
+
+  public openDialog(): void {
+    const dialogRef = this.dialog.open(ModalDialogComponent, {
+      width: '400px',
+      data: {title: 'Add New Board'}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.kanbanService.addBoard(result);
+    });
+  }
+
+}
